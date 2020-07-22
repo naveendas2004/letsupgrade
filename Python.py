7239d77b5a56eda0de9bbf5e9046c1325220c909
naveendas2004 <61614831+naveendas2004@users.noreply.github.com>
2020-07-22 08:19:17 +0530

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,29 @@
+# Assignment of Day-02
+
+#Variables:  
+Syntax: Variable Name = value
+
+number = 1557
+
+#Operators and Assignment Operation and also Formatted Strings
+
+operand1 = 35
+operand2 = 5
+operation = 0
+
+#Addition
+operation = operand1 + operand2
+print(f"Addition Value = {operation} \n") #Escape Sequences
+#Subtraction
+operation = operand1 - operand2
+print(f"Subtracted Value = {operation}")
+#Division
+operation = operand1//operand2 #Returns Integer
+print(f"Divided Value = {operation}")
+#Reminder
+operation = operand1 % operand2
+print(f'Reminder\'s = {operation}') #Back Slash
+#Multipliaction
+operation = operand1 * operand2
+print(f"Multiplied Value = {operation}")
+
