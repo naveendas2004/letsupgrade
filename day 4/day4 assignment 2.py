9001ad26d77fbea2e4a0494aa062a7eeb9d8fe24
naveendas2004 <61614831+naveendas2004@users.noreply.github.com>
2020-07-24 11:22:38 +0530

    Add files via upload

---
+++ day 4/day4 assignment 2.py
@@ -0,0 +1,11 @@
+print("  Program to convert string !! ")
+
+key = input("Enter a sentence: \n")
+
+lower = key.lower()
+
+print(f"Lower case is: {lower} \n")
+
+upper = key.upper()
+
+print(f"Upper case is: {upper} \n")
