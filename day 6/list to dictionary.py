1cd7734eea6187a74618a1314321f71449cd84ee
naveendas2004 <61614831+naveendas2004@users.noreply.github.com>
2020-07-28 20:46:10 +0530

    Add files via upload

---
+++ day 6/list to dictionary.py
@@ -0,0 +1,10 @@
+#program to convert a given list into dictionary
+list1 = [1,2,3,4,5,6,7,8]
+
+list2 = ["a" , "b" , "c" , "d" , "e"]
+
+merged_List = {
+    list1[i] : list2[i] for i in range(len(list2))
+}
+
+print("\nCombined List:\n",merged_List)
