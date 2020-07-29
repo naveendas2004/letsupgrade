ed951db95f064b5bd9c2191306409d3509792b7c
naveendas2004 <61614831+naveendas2004@users.noreply.github.com>
2020-07-29 19:57:41 +0530

    Add files via upload

---
+++ day7/tuplesout.py
@@ -0,0 +1,11 @@
+original tuple = [(1,2,3) , [1,2] , ['a' , 'hit' , 'less']]
+
+ou = [x[0] for x in original tuple]
+
+out1 = [i[1] for i in original tuple]
+
+out2 = [j[-1] for j in origina tuple]
+
+new_List = out + out1 + out2
+
+print(new_List)
